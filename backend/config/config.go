aea7d3fd221560b86e78a6b8cb2d777aea404166
FabioKaelin <supermae206@gmail.com>
2024-05-16 14:34:06 +0200

    add config to backend

---
+++ backend/config/config.go
@@ -0,0 +1,74 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	GinMode string
+
+	DatabaseHost     string
+	DatabaseUser     string
+	DatabasePassword string
+	DatabasePort     string
+
+	FrontEndOrigin string
+
+	JWTTokenSecret string
+)
+
+func getString(key string) (string, error) {
+	value := os.Getenv(key)
+	if value == "" {
+		return "", fmt.Errorf("key '%s' not found", key)
+	}
+	return value, nil
+}
+
+func Load() error {
+	godotenv.Load(".env")
+
+	ginmode, err := getString("GIN_MODE")
+	if err != nil {
+		return err
+	}
+	if ginmode != "debug" && ginmode != "release" {
+		ginmode = "debug"
+	}
+	GinMode = ginmode
+
+	DatabaseHost, err = getString("DATABASE_HOST")
+	if err != nil {
+		return err
+	}
+
+	DatabaseUser, err = getString("DATABASE_USER")
+	if err != nil {
+		return err
+	}
+
+	DatabasePassword, err = getString("DATABASE_PASSWORD")
+	if err != nil {
+		return err
+	}
+
+	DatabasePort, err = getString("DATABASE_PORT")
+	if err != nil {
+		return err
+	}
+
+	FrontEndOrigin, err = getString("FRONT_END_ORIGIN")
+	if err != nil {
+		return err
+	}
+
+	JWTTokenSecret, err = getString("JWT_TOKEN_SECRET")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/fabiokaelin/passowrd-safe/config"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	err := config.Load()
+
+	if err != nil {
+		fmt.Println("Error in Load config", err)
+		panic(err)
+	}
+
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
