4bd3a580db365f90aa0a75e9502addab3632a0cb
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-05-30 16:32:49 +0200

    Navbar is rendered in the Header

--- frontend-safe/src/app/page.tsx
+++ frontend-safe/src/app/page.tsx
@@ -4,11 +4,13 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Navbar from "@/app/components/Navbar";
+import Header from "@/app/components/Header";
 
 export default function Home() {
   return (
-      <><Navbar/>
+      <>
           <div>
+              <Header title={"Hi"} />
               <h1 className="text-red-500">Home</h1>
               <Link href="/login">Go to Login</Link>
           </div>
