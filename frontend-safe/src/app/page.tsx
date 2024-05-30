7747143300c4a8c419e0238d15e94fe0ff5f4750
nxtlvll <lukas.winterleitner@gmail.com>
2024-05-30 16:14:33 +0200

    Navbar component was created and integrated into page.tsx

--- frontend-safe/src/app/page.tsx
+++ frontend-safe/src/app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import Image from "next/image";
-import LogIn from "./pages/LogIn";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import Navbar from "@/app/components/Navbar";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-red-500">Home</h1>
-      <Link href="/login">Go to Login</Link>
-    </div>
+      <><Navbar/>
+          <div>
+              <h1 className="text-red-500">Home</h1>
+              <Link href="/login">Go to Login</Link>
+          </div>
+      </>
   );
 }
