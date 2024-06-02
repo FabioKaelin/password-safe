08deb10722e275b9215d54a4c5d39032b798a4a9
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-06-02 17:57:02 +0200

    password safe shows password as "*", till the see button is pressed

--- frontend-safe/src/app/components/PasswordTable.tsx
+++ frontend-safe/src/app/components/PasswordTable.tsx
@@ -1,13 +1,24 @@
 "use client"
 
 import {VaultEntry} from "@/app/vault/vaultEntry";
-import {retry} from "next/dist/compiled/@next/font/dist/google/retry";
+import React, {useState} from "react";
 
 type PasswordTableProps = {
     entries: VaultEntry[]
 }
 
 export default function PasswordTable({entries}: PasswordTableProps) {
+
+    const [see, setSee] = useState<boolean>(false)
+
+    const getPasswordContent = (entry: VaultEntry): React.JSX.Element => {
+        let password =
+            see ? entry.password
+                : "*".repeat(entry.password.length);
+
+        return <p>{password}</p>
+    }
+
     return (
         <table className="table-fixed">
             <thead>
@@ -28,7 +39,10 @@ export default function PasswordTable({entries}: PasswordTableProps) {
                             <td>{entry.description}</td>
                             <td>{entry.url}</td>
                             <td>{entry.username}</td>
-                            <td>{entry.password}</td>
+                            <td>
+                                {getPasswordContent(entry)}
+                                <button onClick={() => setSee(!see)}>See</button>
+                            </td>
                         </tr>
                     )
                 })
