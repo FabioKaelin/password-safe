8c78402b432c8aa10c55ea76e4c1f912413e0676
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-06-08 21:11:31 +0200

    login modal got new styling

--- frontend-safe/src/app/login/page.tsx
+++ frontend-safe/src/app/login/page.tsx
@@ -1,50 +1,53 @@
 "use client";
 
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Header from "../components/Header";
 import Link from "next/link";
-import { LogInToVault } from "./api";
-import { useRouter } from "next/navigation";
+import {LogInToVault} from "./api";
+import {useRouter} from "next/navigation";
 
 export type User = {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 export default function LogIn() {
-  const [user, setUser] = useState<User>({ email: "", password: "" });
+    const [user, setUser] = useState<User>({email: "", password: ""});
 
-  const router = useRouter();
+    const router = useRouter();
 
-  const handleLogIn = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const resp = await LogInToVault(user);
-      // TODO comment out in end product
-      console.log(resp.json())
-      
-      if (resp.status === 200) {
-        router.push("/vault");
-      } else {
-        router.push("/loginfailed");
-      }
-    } catch (error) {
-      console.error("Error logging in:", error);
+    const handleLogIn = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const resp = await LogInToVault(user);
+            // TODO comment out in end product
+            console.log(resp.json())
+
+            if (resp.status === 200) {
+                router.push("/vault");
+            } else {
+                router.push("/loginfailed");
+            }
+        } catch (error) {
+            console.error("Error logging in:", error);
+        }
     }
-  }
 
-  return (
-    <div>
-      <Header title={"Log In"} />
-      <form className="grid grid-gap-5 grid-row-7 text-black" onSubmit={handleLogIn}>
-        <input type="text" placeholder="Email" onChange={(e) => setUser({ ...user, email: e.target.value })} />
-        <br />
-        <input type="password" placeholder="Password" onChange={(e) => setUser({ ...user, password: e.target.value })} />
-        <br />
-        <button type="submit" className="text-white">Log In</button>
-        <br />
-        <Link href="/register " className="text-white">Sign Up</Link>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <Header title={"Log In"}/>
+            <div className={"flex justify-center items-center"}>
+                <form className="grid gap-5 text-black w-2/5" onSubmit={handleLogIn}>
+                    <input type="text" placeholder="Email"
+                           className={"px-4 py-2 input input-bordered border border-blue-500 rounded w-full"}
+                           onChange={(e) => setUser({...user, email: e.target.value})}/>
+                    <input type="password" placeholder="Password"
+                           className={"px-4 py-2 input input-bordered border border-blue-500 rounded w-full"}
+                           onChange={(e) => setUser({...user, password: e.target.value})}/>
+                    <button type="submit" className="btn bg-teal-400 hover:bg-teal-500 text-black">Log In</button>
+                    <Link href="/register " className="btn bg-teal-400 hover:bg-teal-500 text-black">Sign Up</Link>
+                </form>
+            </div>
+        </div>
+    );
 }
