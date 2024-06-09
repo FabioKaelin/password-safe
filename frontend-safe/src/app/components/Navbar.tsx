e26fded3d8de23b884d84e9c1d2de1c9237ee813
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-06-09 08:16:24 +0200

    Navbar got "Home", "Login" and "register" as link + bg-teal-700 as background color

--- frontend-safe/src/app/components/Navbar.tsx
+++ frontend-safe/src/app/components/Navbar.tsx
@@ -5,24 +5,24 @@ import Logo from "./Logo";
 const Navbar = () => {
     return (
         <>
-            <div className="w-full h-20 bg-purple-800 top-0">
+            <div className="w-full h-20 bg-teal-700 top-0">
                 <div className="container mx-auto px-4 h-full">
                     <div className="flex justify-between items-center h-full">
                         <Logo/>
                         <ul className="hidden md:flex gap-x-6 text-white">
                             <li>
-                                <Link href="/about">
-                                    <p>About Us</p>
+                                <Link href="/">
+                                    <p>Home</p>
                                 </Link>
                             </li>
                             <li>
-                                <Link href="/services">
-                                    <p>Services</p>
+                                <Link href="/login">
+                                    <p>Login</p>
                                 </Link>
                             </li>
                             <li>
-                                <Link href="/contacts">
-                                    <p>Contacts</p>
+                                <Link href="/register">
+                                    <p>Register</p>
                                 </Link>
                             </li>
                         </ul>
