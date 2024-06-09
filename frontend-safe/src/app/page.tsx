17bbc0d3beeb1e808d4ceb69ebddd63e1b1093f8
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-06-09 08:59:52 +0200

    Login and register button are centralized and bigger

--- frontend-safe/src/app/page.tsx
+++ frontend-safe/src/app/page.tsx
@@ -1,25 +1,29 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 import Link from "next/link";
 import Navbar from "@/app/components/layout/Navbar";
 import Header from "@/app/components/layout/Header";
 import React, {useEffect} from "react";
 
 export default function Home() {
-    
-  return (
-      <>
-          <div>
-              <Header title={"Hi"} />
-              <Link href="/login" className="btn bg-teal-400 hover:bg-teal-500 text-black">
-                  Log In
-              </Link>
-              <Link href="/register " className="btn bg-teal-400 hover:bg-teal-500 text-black">
-                  Sign Up
-              </Link>
-          </div>
-      </>
-  );
+
+    return (
+        <>
+            <div>
+                <Header title={"Home"}/>
+                <div className={"flex justify-center items-center min-h-full"}>
+                    <div className={"grid gap-5 text-black w-2/5"}>
+                        <Link href="/login" className="btn bg-teal-400 hover:bg-teal-500 text-black">
+                            Log In
+                        </Link>
+                        <Link href="/register " className="btn bg-teal-400 hover:bg-teal-500 text-black">
+                            Sign Up
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
