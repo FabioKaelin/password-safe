793fcf07555dc54e13d1953456768a82d36281ba
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-06-09 08:23:22 +0200

    Register button is bg-teal-400

--- frontend-safe/src/app/components/modals/NewPasswordModal.tsx
+++ frontend-safe/src/app/components/modals/NewPasswordModal.tsx
@@ -114,7 +114,7 @@ export default function NewPasswordModal({setIsRefresh}: RefreshType) {
                             <br/>
                             <button
                                 type="submit"
-                                className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                                className="px-4 py-2 bg-teal-400 text-black rounded hover:bg-teal-500 rounded "
                             >
                                 Register
                             </button>
