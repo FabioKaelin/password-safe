c60a341ac531c72caaaaf36565be974b871e83e4
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-06-10 08:56:50 +0200

    Pressing the logo on the navbar navigates to the home page

--- frontend-safe/src/app/components/layout/Logo.tsx
+++ frontend-safe/src/app/components/layout/Logo.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const Logo = () => {
     return (
-        <div className={"flex items-center"}>
+        <Link href={"/"} className={"flex items-center"}>
             <Image src="/passwordguy.png" alt="logo" width={"75"} height={"75"}/>
             <h1 className={"text-3xl mx-5 text-white"}>RoboGuard</h1>
-        </div>
+        </Link>
     );
 };
 
