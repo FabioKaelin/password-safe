eaf10c6d4c08f94f2989eb8e9e0d8e8704428682
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-06-11 23:05:29 +0200

    Create new password provides the option to create a new category

--- frontend-safe/src/app/components/modals/NewPasswordModal.tsx
+++ frontend-safe/src/app/components/modals/NewPasswordModal.tsx
@@ -27,7 +27,9 @@ export default function NewPasswordModal({setIsRefresh}: RefreshType) {
     });
     const [categories, setCategories] = useState<Category[]>([])
 
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [isNewCategory, setIsNewCategory] = useState<boolean>(false);
+    const [newCategory, setNewCategory] = useState<Category>({category: ""});
     const [showPassword, setShowPassword] = useState<boolean>(false)
 
     useEffect(() => {
@@ -127,10 +129,10 @@ export default function NewPasswordModal({setIsRefresh}: RefreshType) {
                                   )}
                                </button>
                             </span>
-                            
+
                             <select className="px-4 py-2 select select-bordered border border-blue-500 rounded"
                                     onChange={(e) => setEntry({...entry, category: e.target.value})}>
-                                
+
                                 <option disabled selected value={""}>Select a category if you like</option>
                                 {
                                     categories.map((category) => {
@@ -139,6 +141,51 @@ export default function NewPasswordModal({setIsRefresh}: RefreshType) {
                                     })
                                 }
                             </select>
+                            <div className={"flex items-center justify-center"}>
+                                <button type={"button"} onClick={() => setIsNewCategory(!isNewCategory)}>Create a new
+                                    category
+                                </button>
+                                <div className="dropdown dropdown-end">
+                                    <div tabIndex={0} role="button"
+                                         className="btn btn-circle btn-ghost btn-xs text-info">
+                                        <svg tabIndex={0} xmlns="http://www.w3.org/2000/svg" fill="none"
+                                             viewBox="0 0 24 24" className="w-4 h-4 stroke-current">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                        </svg>
+                                    </div>
+                                    <div tabIndex={0}
+                                         className="card compact dropdown-content z-[1] shadow bg-base-100 rounded-box w-64">
+                                        <div tabIndex={0} className="card-body">
+                                            <h2 className="card-title">Attention!</h2>
+                                            <p>When you add a category it will not be automatically changed in the category modal and not created until you save the password</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {
+                                isNewCategory && (
+                                    <div className={"grid"}>
+                                        <input
+                                            name="Create new Category"
+                                            placeholder="newCategory"
+                                            value={newCategory.category}
+                                            onChange={(e) => setNewCategory({category: e.target.value})}
+                                            className="px-4 py-2 input input-bordered border border-blue-500 rounded"
+                                        />
+                                        <br/>
+                                        <button type={"button"} onClick={() => {
+                                            if (entry.category !== newCategory.category) {
+                                                setCategories([...categories, newCategory])
+                                            }
+                                            setIsNewCategory(!isNewCategory)
+                                        }}>Close modal
+                                        </button>
+                                    </div>
+
+                                )
+                            }
+
                             <br/>
                             <button
                                 type="submit"
