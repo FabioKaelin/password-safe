3c626d57a340a3ba3750fb0b7d1ae36b7d7ed3ea
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-06-11 23:15:13 +0200

    Refactor: Logic for creating categories was moved to CreateNewCategory.tsx

---
+++ frontend-safe/src/app/components/layout/CreateNewCategory.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import {Category} from "@/app/vault/category";
+import {VaultEntry} from "@/app/vault/vaultEntry";
+
+
+export type NewCategoryProps = {
+    isNewCategory: boolean;
+    setIsNewCategory: React.Dispatch<React.SetStateAction<boolean>>;
+    newCategory: Category;
+    setNewCategory: React.Dispatch<React.SetStateAction<Category>>
+    categories: Category[];
+    setCategories: React.Dispatch<React.SetStateAction<Category[]>>;
+    entry: VaultEntry;
+}
+export default function CreateNewCategory({
+                                              isNewCategory,
+                                              setIsNewCategory,
+                                              newCategory,
+                                              setNewCategory,
+                                              categories,
+                                              setCategories,
+                                              entry
+                                          }: NewCategoryProps) {
+
+    return (
+        <div>
+            <div className={"flex items-center justify-center"}>
+                <button type={"button"} onClick={() => setIsNewCategory(!isNewCategory)}>Create a new
+                    category
+                </button>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button"
+                         className="btn btn-circle btn-ghost btn-xs text-info">
+                        <svg tabIndex={0} xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 24 24" className="w-4 h-4 stroke-current">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                    </div>
+                    <div tabIndex={0}
+                         className="card compact dropdown-content z-[1] shadow bg-base-100 rounded-box w-64">
+                        <div tabIndex={0} className="card-body">
+                            <h2 className="card-title">Attention!</h2>
+                            <p>When you add a category it will not be automatically changed in the category modal and
+                                not
+                                created until you save the password</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {
+                isNewCategory && (
+                    <div className={"grid"}>
+                        <input
+                            name="newCategory"
+                            placeholder="Create new Category"
+                            value={newCategory.category}
+                            onChange={(e) => setNewCategory({category: e.target.value})}
+                            className="px-4 py-2 input input-bordered border border-blue-500 rounded"
+                        />
+                        <br/>
+                        <button type={"button"} onClick={() => {
+                            if (entry.category !== newCategory.category) {
+                                setCategories([...categories, newCategory])
+                            }
+                            setNewCategory({category: ""})
+                            setIsNewCategory(!isNewCategory)
+                        }}>Close modal
+                        </button>
+                    </div>
+
+                )
+            }
+        </div>
+    )
+
+}
--- frontend-safe/src/app/components/modals/NewPasswordModal.tsx
+++ frontend-safe/src/app/components/modals/NewPasswordModal.tsx
@@ -8,6 +8,7 @@ import Router from "next/router";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
 import {Category, GetAllCategoriesFromVault} from "@/app/vault/category";
+import CreateNewCategory from "@/app/components/layout/CreateNewCategory";
 
 
 export type RefreshType = {
@@ -141,50 +142,11 @@ export default function NewPasswordModal({setIsRefresh}: RefreshType) {
                                     })
                                 }
                             </select>
-                            <div className={"flex items-center justify-center"}>
-                                <button type={"button"} onClick={() => setIsNewCategory(!isNewCategory)}>Create a new
-                                    category
-                                </button>
-                                <div className="dropdown dropdown-end">
-                                    <div tabIndex={0} role="button"
-                                         className="btn btn-circle btn-ghost btn-xs text-info">
-                                        <svg tabIndex={0} xmlns="http://www.w3.org/2000/svg" fill="none"
-                                             viewBox="0 0 24 24" className="w-4 h-4 stroke-current">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                        </svg>
-                                    </div>
-                                    <div tabIndex={0}
-                                         className="card compact dropdown-content z-[1] shadow bg-base-100 rounded-box w-64">
-                                        <div tabIndex={0} className="card-body">
-                                            <h2 className="card-title">Attention!</h2>
-                                            <p>When you add a category it will not be automatically changed in the category modal and not created until you save the password</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            {
-                                isNewCategory && (
-                                    <div className={"grid"}>
-                                        <input
-                                            name="Create new Category"
-                                            placeholder="newCategory"
-                                            value={newCategory.category}
-                                            onChange={(e) => setNewCategory({category: e.target.value})}
-                                            className="px-4 py-2 input input-bordered border border-blue-500 rounded"
-                                        />
-                                        <br/>
-                                        <button type={"button"} onClick={() => {
-                                            if (entry.category !== newCategory.category) {
-                                                setCategories([...categories, newCategory])
-                                            }
-                                            setIsNewCategory(!isNewCategory)
-                                        }}>Close modal
-                                        </button>
-                                    </div>
 
-                                )
-                            }
+                            <CreateNewCategory isNewCategory={isNewCategory} setIsNewCategory={setIsNewCategory}
+                                               newCategory={newCategory} setNewCategory={setNewCategory}
+                                               categories={categories} setCategories={setCategories}
+                                               entry={entry}/>
 
                             <br/>
                             <button
