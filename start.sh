8a1246632d939b9a4f28c48c310958bd41813151
FabioKaelin <supermae206@gmail.com>
2024-06-13 16:40:25 +0200

    add start script

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd resources
+
+docker compose up -d
+
+cd ..
+
+cd frontend-safe
+
+npm run dev &
+
+cd ..
+
+cd backend
+
+cp .env.sample .env
+
+go run main.go
+
+echo "open localhost:3001 in your browser"
