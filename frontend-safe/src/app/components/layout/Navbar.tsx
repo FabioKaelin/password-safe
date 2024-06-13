5ad3d678cdfaaa499d29c523cfe4ce8a77fe449a
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-06-13 10:44:42 +0200

    Text of navbar is white

--- frontend-safe/src/app/components/layout/Navbar.tsx
+++ frontend-safe/src/app/components/layout/Navbar.tsx
@@ -31,31 +31,10 @@ const Navbar = () => {
         setUser({id: "", email: "", password: ""})
         router.push("/login")
     }
-
-    <div className="navbar bg-base-100">
-        <div className="flex-1">
-            <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-            <ul className="menu menu-horizontal px-1">
-                <li><a>Link</a></li>
-                <li>
-                    <details>
-                        <summary>
-                            Parent
-                        </summary>
-                        <ul className="p-2 bg-base-100 rounded-t-none">
-                            <li><a>Link 1</a></li>
-                            <li><a>Link 2</a></li>
-                        </ul>
-                    </details>
-                </li>
-            </ul>
-        </div>
-    </div>
+    
     return (
         <>
-            <div className="w-full h-20 bg-teal-700 top-0">
+            <div className="w-full h-20 bg-teal-700 top-0 text-white">
                 <div className="container mx-auto px-4 h-full">
                     <div className="flex justify-between items-center h-full">
                         <Logo/>
